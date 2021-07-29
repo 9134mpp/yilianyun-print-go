81476a073bfc5ed49ddfb1a122f3ab6a15447b17
yly <1050463330@qq.com>
2021-07-29 14:40:46 +0800

    优化body问题

--- Demo/client/main.go
+++ Demo/client/main.go
@@ -76,15 +76,15 @@ func foreign(){
 	// 这里示范个人推荐的授权模式 (极速授权模式：http://doc2.10ss.net/343932)
 	//授权
 	pam := &pd.ForeignOauthRequest{
-		MachineCode: "4004628156",
-		Msign: "14757845145",
+		MachineCode: "",
+		Msign: "",
 	}
 	token, _ := Api.GetForeignToken(Client, pam) // 这里token存在数据库或者缓存中即可
 	fmt.Println(token)
 	//打印
 	p := &pd.PrintRequest{
 		AccessToken: token,
-		MachineCode: "4004628158",
+		MachineCode: "",
 		Content:     content(),
 	}
 	_ = Api.Print(Client, p)
